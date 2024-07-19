11ae449f511d128ebffb195429ca1e3f6a8016db
IRAM <iramkhizar118@gmail.com>
2024-07-19 05:41:31 -0700

    countdown

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import "./App.css";
+import Countdown from "./components/countdown/countdown";
+
+function App() {
+  return (
+    <div>
+      <main>
+        <Countdown />
+      </main>
+    </div>
+  );
+}
+
+export default App;
